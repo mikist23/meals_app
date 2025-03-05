c0ee948fdbeb2afbfa88970a2bae7bb230afa1fe
@Mikist <mikeochango3@gmail.com>
2025-03-05 10:21:26 +0300

    add home vue vew

--- meals-app-main/src/views/Home.vue
+++ meals-app-main/src/views/Home.vue
@@ -1,4 +1,4 @@
-<!-- <template>
+<template>
   <div class="p-8 pb-0 text-orange-500">
     <h1 class="text-4xl font-bold mb-4">Random Meals</h1>
   </div>
@@ -20,4 +20,4 @@ onMounted(async () => {
       .then(({ data }) => meals.value.push(data.meals[0]));
   }
 });
-</script> -->
+</script>
