b3841fbebae1ef7f042c2f7155d4fb39974984cb
@Mikist <mikeochango3@gmail.com>
2025-03-05 10:23:27 +0300

    add meal by letter view

--- meals-app-main/src/views/MealsByLetter.vue
+++ meals-app-main/src/views/MealsByLetter.vue
@@ -1,4 +1,4 @@
-<!-- <template>
+<template>
   <div class="p-8 pb-0">
     <h1 class="text-4xl font-bold mb-4 text-orange-500">Meals by Letter</h1>
   </div>
@@ -34,4 +34,4 @@ watch(route, () => {
 onMounted(() => {
   store.dispatch("searchMealsByLetter", route.params.letter);
 });
-</script> -->
+</script>
