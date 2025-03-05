a3f06917159c6024a66099dbfd605cd4d8285801
@Mikist <mikeochango3@gmail.com>
2025-03-05 10:22:58 +0300

    add mmeal by ingridient view

--- meals-app-main/src/views/MealsByIngredient.vue
+++ meals-app-main/src/views/MealsByIngredient.vue
@@ -1,4 +1,4 @@
-<!-- <template>
+<template>
   <div class="p-8 pb-0">
     <h1 class="text-4xl font-bold mb-4 text-orange-500">Meals for {{ ingredient.strIngredient }}</h1>
   </div>
@@ -21,4 +21,4 @@ onMounted(() => {
   store.dispatch('searchMealsByIngredient', route.params.ingredient)
 })
 
-</script> -->
+</script>
