f28c2b08ac0faf73c63d4b70341f1d478908070d
@Mikist <mikeochango3@gmail.com>
2025-03-05 10:21:54 +0300

    add ingridients view

--- meals-app-main/src/views/Ingredients.vue
+++ meals-app-main/src/views/Ingredients.vue
@@ -1,4 +1,4 @@
-<!-- <template>
+<template>
   <div class="p-8 pb-0">
     <h1 class="text-4xl font-bold mb-4 text-orange-500">Ingredients</h1>
   </div>
@@ -52,4 +52,4 @@ onMounted(() => {
     ingredients.value = data.meals;
   });
 });
-</script> -->
+</script>
